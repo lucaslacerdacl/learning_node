f19b2e67111801701b23ea1934cf491e151e4db0
Lucas <lucas.lacerda@gmail.com>
2017-04-14 16:45:52 -0300

    Add express-validator on project

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -7,6 +7,20 @@ var adminRoutes = function(app){
 		var connection = app.config.dbConnection();
 		var newsModel = app.app.models.newsModel();
 
+		request.assert('title', 'Título is required').notEmpty();
+		request.assert('resume', 'Resume is required').notEmpty();
+		request.assert('resume', 'Resume deve conter entre 10 e 100 characters').len(10, 100);
+		request.assert('author_name', 'Author is required').notEmpty();
+		request.assert('notice_date', 'Notice date is required').notEmpty().isDate({format: 'YYYY-MM-DD'});
+		request.assert('notice', 'Notice is required').notEmpty();
+
+		var errors = request.validationErrors();
+
+		if(errors){
+			response.render('admin/form_add_notice');
+			return;
+		}
+
 		newsModel.addNotice(request.body, connection, function(error, result){
 			response.redirect('/news');
 		});
