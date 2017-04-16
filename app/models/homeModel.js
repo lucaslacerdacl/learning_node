cb05a593327062ad1b3457e0a8019cb51706a7a0
Lucas <lucas.lacerda@gmail.com>
2017-04-16 19:19:21 -0300

    fix query

--- app/models/homeModel.js
+++ app/models/homeModel.js
@@ -1,7 +1,7 @@
 var homeModel = function(){
 
 	this.getNews = function(connection, callback){
-		connection.query('select * from news order by date_creation desc limit 5', callback);
+		connection.query('select * from news order by date_creation desc limit 6', callback);
 	}
 
 	return this;
